41a1a7fcb08dbceabf79db93d74055cb8f809fd4
IT21118968 <100430225+IT21118968@users.noreply.github.com>
2022-05-20 22:05:53 +0530

    Create Main.cpp

---
+++ Main.cpp
@@ -0,0 +1,17 @@
+#include "guestUser.h"
+#include "registeredUser.h"
+#include "buyer.h"
+#include "seller.h"
+#include "property.h"
+#include "agent.h"
+#include "purchasing.h"
+#include "selling.h
+#include "payment.h"
+
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+guestUser*gu = new registerClient();
+registeredUser*rgu = new  
